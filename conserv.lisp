56d8c26d53befabe46ba6e3d4b1797c0d7153b8c
Kat Marchán <kzm@sykosomatic.org>
2012-02-09 20:59:33 -0500

    Working writes.

--- conserv.lisp
+++ conserv.lisp
@@ -18,6 +18,21 @@
 (defevent on-client-error (driver client error))
 (defevent on-drain (driver client))
 
+(defun make-queue () (cons nil nil))
+
+(defun enqueue (obj q)
+  (if (null (car q))
+      (setf (cdr q) (setf (car q) (list obj)))
+      (setf (cdr (cdr q)) (list obj)
+            (cdr q) (cdr (cdr q))))
+  (car q))
+
+(defun dequeue (q)
+  (pop (car q)))
+
+(defun queue-empty-p (q)
+  (null (car q)))
+
 (defparameter *max-buffer-size* 16384)
 (defstruct (tcp-client
              (:constructor make-client (socket remote-name remote-port
@@ -26,7 +41,9 @@
   remote-name
   remote-port
   (read-buffer (make-array buffer-size :element-type '(unsigned-byte 8)))
-  (write-buffer (make-array buffer-size :element-type '(unsigned-byte 8))))
+  (write-queue (make-queue))
+  write-buffer
+  write-buffer-offset)
 
 (defclass tcp-server ()
   ((driver :initarg :driver)))
@@ -57,7 +74,8 @@
                                                (setf (gethash (cons name port) connections)
                                                      client)
                                                (on-connect driver client)
-                                               (start-reads driver event-base client))))))
+                                               (start-reads driver event-base client)
+                                               (start-writes driver event-base client))))))
            (handler-case
                (iolib:event-dispatch event-base)
              ((or iolib:socket-connection-reset-error iolib:hangup end-of-file) (e)
@@ -86,25 +104,43 @@
   (declare (ignore driver))
   (iolib:remove-fd-handlers event-base (iolib:socket-os-fd (tcp-client-socket client)) :read t))
 
-(defun start-writes (driver event-base client &aux (buffer (tcp-client-write-buffer client)))
-  ;; TODO - this blows and doesn't work. Make it not blow.
-  (iolib:set-io-handler event-base (iolib:socket-os-fd (tcp-client-socket client))
-                        :write
-                        (lambda (&rest ig)
-                          (declare (ignore ig))
-                          (handler-case
-                              (let ((bytes-written (iolib:send-to (tcp-client-socket client) buffer)))
-                                (when t ; (buffer-drained-p buffer)
-                                  (on-drain driver client)))
-                            ((or iolib:socket-connection-reset-error isys:ewouldblock isys:epipe) (e)
-                              (on-client-error driver client e))))))
+(defun start-writes (driver event-base client)
+  (iolib:set-io-handler
+   event-base (iolib:socket-os-fd (tcp-client-socket client))
+   :write
+   (lambda (&rest ig)
+     (declare (ignore ig))
+     (loop
+        (unless (tcp-client-write-buffer client)
+          (setf (tcp-client-write-buffer client) (dequeue (tcp-client-write-queue client))
+                (tcp-client-write-buffer-offset client) 0))
+        (when (tcp-client-write-buffer client)
+          (handler-case
+              (let ((bytes-written (iolib:send-to (tcp-client-socket client)
+                                                  (tcp-client-write-buffer client)
+                                                  :start (tcp-client-write-buffer-offset client))))
+
+                (when (>= (incf (tcp-client-write-buffer-offset client) bytes-written)
+                          (length (tcp-client-write-buffer client)))
+                  (setf (tcp-client-write-buffer client) nil)
+                  (when (queue-empty-p (tcp-client-write-queue client))
+                    (on-drain driver client))))
+            ((or iolib:socket-connection-reset-error isys:ewouldblock isys:epipe) (e)
+              (on-client-error driver client e))))
+        (when (queue-empty-p (tcp-client-write-queue client))
+          (return))))))
+
+(defun write-to-client (client data)
+  (enqueue data (tcp-client-write-queue client)))
 
 ;; TCP server test
 (defclass echo () ())
 (defmethod on-connect ((driver echo) client)
-  (format t "~&Client connected."))
+  (format t "~&Client connected.")
+  (write-to-client client (flex:string-to-octets (format nil "~&Welcome. Everything you say will be echoed back to you now.~%"))))
 (defmethod on-data ((driver echo) client data)
-  (format t "~&Data length: ~S~%" (length data)))
+  (format t "~&Data length: ~S~%" (length data))
+  (write-to-client client data))
 (defmethod on-disconnect ((driver echo) client)
   (format t "~&Client disconnected: ~S~%" client))
 #+nil(start-listener (make-tcp-server (make-instance 'echo)) :host "127.0.0.1" :port 1337 :external-format :utf8)
