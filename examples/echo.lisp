bfc9f5363419a83fd243d83dc67542f1cc8d5bef
Kat Marchán <kzm@sykosomatic.org>
2012-02-15 20:54:04 -0500

    Minimized echo server.

--- examples/echo.lisp
+++ examples/echo.lisp
@@ -2,16 +2,17 @@
   (:use :cl :conserv))
 (in-package #:conserv-echo)
 
-;; Mostly-bare-minimum echo server
+;; 'Bare-minimum' echo server
 (defclass echo-test () ())
-(defmethod on-tcp-server-connection ((driver echo-test) server client)
-  (format t "~&Client connected: ~S~%" client)
-  (format client "~&Hello. Welcome to the server (~S).~%" server))
+
 (defmethod on-socket-data ((driver echo-test) socket data)
   (write-sequence data socket))
+
 (defun basic-echo ()
   (with-event-loop ()
-    (server-listen (make-instance 'echo-test) :host "127.0.0.1" :port 1337)))
+    (server-listen (make-instance 'echo-test)
+                   :host "127.0.0.1"
+                   :port 1337)))
 
 ;; Local IPC sockets!
 (defun ipc-socket-test ()
@@ -24,6 +25,10 @@
 ;; To take this much higher than this number, you'll likely need to mess with file descriptor
 ;; limits.
 ;; See http://www.xenoclast.org/doc/benchmark/HTTP-benchmarking-HOWTO/node7.html#SECTION00072100000000000000
+(defclass scalability (echo-test) ())
+(defmethod on-tcp-server-connection ((driver scalability) server client)
+  ;; This will set the clients into an echo loop with the server.
+  (format client "~&Welcome to ~A. Please echo this back.~%" server))
 (defparameter *max-echo-clients*  500)
 (defun super-echo ()
   (let ((driver (make-instance 'echo-test)))
