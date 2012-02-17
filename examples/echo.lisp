e4dc888c1c99770c28418843c59b0fd071352f7e
Kat Marchán <kzm@sykosomatic.org>
2012-02-16 22:59:42 -0500

    More putzing around with examples.

--- examples/echo.lisp
+++ examples/echo.lisp
@@ -25,20 +25,24 @@
 ;; To take this much higher than this number, you'll likely need to mess with file descriptor
 ;; limits.
 ;; See http://www.xenoclast.org/doc/benchmark/HTTP-benchmarking-HOWTO/node7.html#SECTION00072100000000000000
-(defparameter *max-echo-clients*  500)
+(defparameter *max-echo-clients* 10)
+(defvar *num-clients*)
 (defclass scalability (echo-test) ())
 (defmethod on-server-connection ((driver scalability) server client)
   ;; This will set the clients into an echo loop with the server.
-  (format client "~&Welcome to ~A. Please echo this back.~%" server))
+  (format client "~&Welcome to ~A. Please echo this back.~%" server)
+  (incf *num-clients*))
 (defvar *total-exchanges*)
 (defmethod on-socket-data :after ((driver scalability) socket data)
   (declare (ignore socket data))
   (incf *total-exchanges*))
 (defmethod on-server-close ((driver scalability) server)
+  (print *num-clients*)
   (print *total-exchanges*))
 (defun super-echo ()
   (let ((driver (make-instance 'scalability))
-        (*total-exchanges* 0))
+        (*total-exchanges* 0)
+        (*num-clients* 0))
     (with-event-loop ()
       (server-listen driver :port 1338)
       (loop repeat *max-echo-clients* do
