0a8ed96f05a995e8f4c593278eac8158d320dcdc
Kat Marchán <kzm@sykosomatic.org>
2012-02-16 20:43:44 -0500

    More useful scalability test.

--- examples/echo.lisp
+++ examples/echo.lisp
@@ -25,14 +25,22 @@
 ;; To take this much higher than this number, you'll likely need to mess with file descriptor
 ;; limits.
 ;; See http://www.xenoclast.org/doc/benchmark/HTTP-benchmarking-HOWTO/node7.html#SECTION00072100000000000000
+(defparameter *max-echo-clients*  500)
 (defclass scalability (echo-test) ())
 (defmethod on-server-connection ((driver scalability) server client)
   ;; This will set the clients into an echo loop with the server.
   (format client "~&Welcome to ~A. Please echo this back.~%" server))
-(defparameter *max-echo-clients*  500)
+(defvar *total-exchanges*)
+(defmethod on-socket-data :after ((driver scalability) socket data)
+  (declare (ignore socket data))
+  (incf *total-exchanges*))
+(defmethod on-server-close ((driver scalability) server)
+  (print *total-exchanges*))
 (defun super-echo ()
-  (let ((driver (make-instance 'echo-test)))
+  (let ((driver (make-instance 'scalability))
+        (*total-exchanges* 0))
     (with-event-loop ()
       (server-listen driver :port 1338)
       (loop repeat *max-echo-clients* do
-           (socket-connect (make-instance 'echo-test) "127.0.0.1" :port 1338 :wait nil)))))
+           (socket-connect driver "127.0.0.1" :port 1338 :wait nil))
+      (exit-event-loop :delay 20))))
