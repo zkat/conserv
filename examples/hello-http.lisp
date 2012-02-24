257a370e12310960e56622a68dde9ed9d78cdf80
Kat Marchán <kzm@sykosomatic.org>
2012-02-24 00:14:45 -0500

    Modified http example.

--- examples/hello-http.lisp
+++ examples/hello-http.lisp
@@ -5,9 +5,9 @@
 (defclass hello-http () ())
 
 (defmethod on-http-request ((driver hello-http))
-  ;; TODO - keyword headers? (setf (reply-header* :content-type) "text/html")
-  (setf (reply-headers*) '(("Content-type" . "text/html")))
-  (format *reply* "<h1>Hello, world!</h1>~%")
+  (setf (reply-headers*) '((:content-type . "text/html")
+                           (:transfer-encoding . "chunked")))
+  (format *reply* "<h1>Hello, world!</h1>")
   (close *reply*))
 
 (defun start ()
