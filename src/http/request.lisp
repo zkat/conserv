768519cb6364bb1e2333bcdf0ecac0247c7112c7
Kat Marchán <kzm@sykosomatic.org>
2012-02-26 09:52:59 -0500

    Docstrings for request protocols.

--- src/http/request.lisp
+++ src/http/request.lisp
@@ -4,27 +4,57 @@
 
 (defprotocol request-event-driver (a)
   ((data ((driver a) data)
-    :default-form nil)
+    :default-form nil
+    :documentation "Event called when *REQUEST* has received data from the user-agent. DATA will be
+                    automatically encoded according to (request-external-format *request*).")
    (continue ((driver a))
     :default-form nil)
    (upgrade ((driver a) data)
-    :default-form (close *request* :abort t))
+    :default-form (close *socket* :abort t)
+    :documentation "Event called when *REQUEST* has received either an Upgrade: header, or a CONNECT
+                    method in an HTTP request. When this happens, *request*'s socket is deregistered
+                    with the HTTP server. The socket is available through the *socket* variable. By
+                    default, this event immediately shuts down the socket.")
    (close ((driver a))
-    :default-form nil))
+    :default-form nil
+    :documentation "Event called when *REQUEST* has been closed."))
   (:prefix on-request-))
 
 (defprotocol request (a)
-  ((state ((request a)) :accessorp t)
-   (method ((request a)) :accessorp t)
-   (url ((request a)) :accessorp t)
-   (headers ((request a)) :accessorp t)
+  ((state ((request a))
+    :accessorp t
+    :documentation "Internal -- keeps track of the internal state of the request, :headers, :body,
+                    or :closed")
+   (method ((request a))
+    :accessorp t
+    :documentation "Request method as a string (e.g. \"GET\", \"POST\", \"CONNECT\")")
+   (url ((request a))
+    :accessorp t
+    :documentation "Request URL as a string (e.g. \"/\", \"/blog/post?number=1\")")
+   (headers ((request a))
+    :accessorp t
+    :documentation "An alist of (name . value) pairs. Header names are :keywords. All values are
+                    strings.")
    #+nil(trailers ((request a)))
-   (http-version ((request a)) :accessorp t)
-   (external-format ((request a)) :accessorp t)
-   (request-parser ((request a)))
-   (http-server ((reply a)))
-   (keep-alive-p ((reply a)) :accessorp t)
-   (socket ((request a))))
+   (http-version ((request a))
+    :accessorp t
+    :documentation "HTTP version as a string. (e.g. \"1.1\", \"1.0\")")
+   (external-format ((request a))
+    :accessorp t
+    :documentation "External format used to encode incoming data. If nil, no encoding will be done,
+                    and ON-REQUEST-DATA will receive arrays of (unsigned-byte 8) as its data.")
+   (request-parser ((request a))
+    :documentation "Internal -- Request parser object.")
+   (http-server ((request a))
+    :documentation "HTTP server associated with this request.")
+   (keep-alive-p ((request a))
+    :accessorp t
+    :documentation "Controls whether the underlying connection should be closed after this request
+                    is completed. For HTTP/1.1 clients, this defaults to true. For other clients,
+                    the default is nil. This value should not be set directly by users, instead, set
+                    the outgoing Connection: close header in this request's reply.")
+   (socket ((request a))
+    :documentation "TCP socket associated with this request."))
   (:prefix request-))
 
 (defclass request ()
