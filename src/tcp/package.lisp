42937275bded9858912cfc4d563a54238578cc68
Kat Marchán <kzm@sykosomatic.org>
2012-02-27 00:08:10 -0500

    Tweaking conserv.tcp exports.

--- src/tcp/package.lisp
+++ src/tcp/package.lisp
@@ -9,19 +9,18 @@
    ;; Sockets
    #:*socket*
    #:on-socket-error
+   #:on-socket-end-of-file
    #:on-socket-connect
    #:on-socket-data
    #:on-socket-close
    #:on-socket-output-empty
    #:drop-connection
 
-   #:*max-buffer-size*
+   #:socket-connect
    #:socket-driver
    #:socket-server
    #:socket-external-format-in
    #:socket-external-format-out
-   #:socket-binary-p
-   #:socket-connect
    #:socket-local-p
    #:socket-remote-name
    #:socket-remote-port
@@ -40,13 +39,11 @@
    #:on-server-error
    #:on-server-close
 
+   #:server-listen
    #:server-driver
-   #:server-socket
    #:server-external-format-in
    #:server-external-format-out
-   #:server-binary-p
    #:server-list-clients
    #:server-count-clients
    #:server-pause
-   #:server-resume
-   #:server-listen))
+   #:server-resume))
