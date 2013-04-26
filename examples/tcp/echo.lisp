352396e0c7ed257ca6a1498a42d12b66afb5d643
Kat Marchán <kzm@sykosomatic.org>
2013-04-25 20:06:30 -0400

    Fixing the echo server example

--- examples/tcp/echo.lisp
+++ examples/tcp/echo.lisp
@@ -3,18 +3,18 @@
   (:import-from #:conserv
                 #:with-event-loop)
   (:import-from #:conserv.tcp
-                #:*socket*
-                #:server-listen
-                #:on-socket-data))
+                #:*tcp-client*
+                #:tcp-listen
+                #:on-tcp-client-data))
 (in-package #:conserv.examples.tcp.echo)
 
 (defclass echo () ())
 
-(defmethod on-socket-data ((driver echo) data)
-  (write-sequence data *socket*))
+(defmethod on-tcp-client-data ((driver echo) data)
+  (write-sequence data *tcp-client*))
 
 (defun start ()
   (with-event-loop ()
-    (server-listen (make-instance 'echo)
-                   :host "127.0.0.1"
-                   :port 1337)))
+    (tcp-listen (make-instance 'echo)
+                :host "127.0.0.1"
+                :port 1337)))
